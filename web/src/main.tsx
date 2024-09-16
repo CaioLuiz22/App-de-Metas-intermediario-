79d0def1ab3d5be8e76ba8323bc459c0b701db4b
CaioLuiz22 <caio3.firmino@gmail.com>
2024-09-16 17:26:24 -0300

    update main

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <App />
